cceb31f315fe61cd3b70202f7a4755df691de1b2
Surya Tikkani <surya_p2010@hotmail.com>
2021-10-07 12:25:07 -0500

    Added comments
    Signed-off-by: Surya Tikkani <surya_p2010@hotmail.com>

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My Second Change.
+        // My Second Change Yes.
         [Key]
         public int CartItemId { get; set; }
 
